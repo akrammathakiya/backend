8e874bccae72d49752a1916eeb753aed263a98da
himanshu <himanshupadecha@gmail.com>
2024-09-29 18:53:31 +0530

    error fixed

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -4,6 +4,9 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs"
 
 
+
+
+
 cloudinary.config({
     cloud_name:process.env.CLOUD_NAME,
     api_key:process.env.CLOUDINARY_API_KEY,

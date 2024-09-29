0b3c59d5604405ffaf09500defae898ac34db7c5
himanshu <himanshupadecha@gmail.com>
2024-09-29 18:49:35 +0530

    error fixed

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,8 @@
+import { config } from "dotenv";
+config()
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs"
+
 
 cloudinary.config({
     cloud_name:process.env.CLOUD_NAME,
@@ -7,12 +10,28 @@ cloudinary.config({
     api_secret:process.env.CLOUDINARY_API_SECRET
 })
 
+// cloudinary.config({
+//     cloud_name: "himanshupadecha",
+//     api_key: "187479743714649",
+//     api_secret: "HKFj7pKq80MOWny682zvNoprmSc"
+// });
+
+
 export const uploadOnCloudinary = async(localpath)=>{
+    console.log("api key :" , process.env.CLOUDINARY_API_KEY);
+    console.log("cloud name :" , process.env.CLOUD_NAME);
+    console.log("api secret :" , process.env.CLOUDINARY_API_SECRET);
+    console.log(cloudinary.config());
+    
+    
     try {
         if(!localpath) return null
         const response = await cloudinary.uploader.upload(localpath,{resource_type:"auto"})
+
         console.log("uploaded successfully");
+
         fs.unlinkSync(localpath)
+
         return response
     } catch (error) {
         fs.unlinkSync(localpath)

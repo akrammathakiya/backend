e2038b66d678a0ce885743a23910df0cd40d603e
akram <mathakiyaakram@gmail.com>
2024-10-08 10:18:05 +0530

    errorfixed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -41,7 +41,7 @@ router.route("/change-password").post(verifyJWT,changeCurrentPassword);
 router.route("/current-user").get(verifyJWT,getCurrentUser);
 router.route("/update-details").patch(verifyJWT,updateAccountDetails);
 router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateAvatar);
-router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),updateCoverImage);
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateCoverImage);
 router.route("/c/:username").get(verifyJWT,getUserChannelProfile);
 router.route("history").get(verifyJWT,getUserWatchHistory);
 
